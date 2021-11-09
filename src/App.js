151c12a93d73708a3010c15fb30d99752ce8aa0b
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2021-11-09 15:13:39 +0100

    feat: add Routing to the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+import AdminPage from "./components/pages/AdminPage";
 import HomePage from "./components/pages/HomePage";
 import MainLayout from "./components/templates/MainLayout";
 
 export default function App() {
   return (
     <MainLayout>
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/admin" element={<AdminPage />} />
+      </Routes>
     </MainLayout>
   );
 }
--- src/components/organisims/Header.jsx
+++ src/components/organisims/Header.jsx
@@ -1,7 +1,14 @@
+import { Link } from "react-router-dom";
+
 export default function Header() {
   return (
     <header className="bg-gray-800 px-4 text-white flex justify-between items-center p-3">
-      Header
+      <Link className="hover:text-yellow-400 " to="/">
+        React Shopping Cart
+      </Link>
+      <Link className="hover:text-yellow-400 mr-4" to="/admin">
+        Admin
+      </Link>
     </header>
   );
 }
