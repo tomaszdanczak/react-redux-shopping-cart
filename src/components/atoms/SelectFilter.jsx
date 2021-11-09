4e69af0df3511d22a0f106e3328bd60c2b580c63
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2021-11-09 16:04:08 +0100

    feat: create simple <SelectFilter/>

---
+++ src/components/atoms/SelectFilter.jsx
@@ -0,0 +1,16 @@
+export default function SelectFilter() {
+  return (
+    <p>
+      Filter
+      <select>
+        <option value="">ALL</option>
+        <option value="XS">XS</option>
+        <option value="S">S</option>
+        <option value="M">M</option>
+        <option value="L">L</option>
+        <option value="XL">XL</option>
+        <option value="XXL">XXL</option>
+      </select>
+    </p>
+  );
+}
