ce99de1365af755d1284b4073fba1cd0abcf778e
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2021-11-09 15:47:38 +0100

    feat: render <Product/> components using DUMMY DATA

--- src/components/organisms/Products.jsx
+++ src/components/organisms/Products.jsx
@@ -1,3 +1,14 @@
+import Product from "../molecules/Product";
+import { products } from "./../../data.json";
+
 export default function Products() {
-  return <div className="col-span-5 ml-4">Products</div>;
+  return (
+    <div className="col-span-5 ml-4">
+      <ul className="grid grid-cols-2 gap-x-4 gap-y-8 lg:grid-cols-3">
+        {products.map((product) => (
+          <Product product={product} key={product._id} />
+        ))}
+      </ul>
+    </div>
+  );
 }
