60ab43a6125adbc9adc33d7a5a5ff99aa49b7e77
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2021-11-09 16:05:05 +0100

    feat: create <FilterSection/> and add it to <Products/>

--- src/components/organisms/Products.jsx
+++ src/components/organisms/Products.jsx
@@ -1,9 +1,11 @@
+import FilterSection from "../molecules/FilterSection";
 import Product from "../molecules/Product";
 import { products } from "./../../data.json";
 
 export default function Products() {
   return (
     <div className="col-span-5 ml-4">
+      <FilterSection />
       <ul className="grid grid-cols-2 gap-x-4 gap-y-8 lg:grid-cols-3">
         {products.map((product) => (
           <Product product={product} key={product._id} />
