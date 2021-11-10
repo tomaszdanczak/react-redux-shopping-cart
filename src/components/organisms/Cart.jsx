fe1d1d5398662e23a40a4cf6330f6caa7de148ad
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2021-11-10 13:30:18 +0100

    feat: add header to the <Cart/> (use DUMMY DATA)

--- src/components/organisms/Cart.jsx
+++ src/components/organisms/Cart.jsx
@@ -1,8 +1,10 @@
 import CartItems from "./CartItems";
+import { cartItems } from "./../../data.json";
 
 export default function Cart() {
   return (
     <div className="col-span-2 ml-4 mr-4">
+      <p className="mb-4 pb-4 border-b border-gray-500">{`You have ${cartItems.length} in the cart.`}</p>
       <CartItems />
     </div>
   );
