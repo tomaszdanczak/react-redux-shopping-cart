4b0bb7e9a7241f6ce838679ea238246f64dcaa29
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2021-11-10 13:57:19 +0100

    feat: create and add to the project <ProceedOrder /> component

--- src/components/organisms/Cart.jsx
+++ src/components/organisms/Cart.jsx
@@ -1,11 +1,13 @@
 import CartItems from "./CartItems";
 import { cartItems } from "./../../data.json";
+import ProceedOrder from "../molecules/ProceedOrder";
 
 export default function Cart() {
   return (
     <div className="col-span-2 ml-4 mr-4">
       <p className="mb-4 pb-4 border-b border-gray-500">{`You have ${cartItems.length} in the cart.`}</p>
       <CartItems />
+      <ProceedOrder />
     </div>
   );
 }
