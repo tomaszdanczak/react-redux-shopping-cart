f660f6389187431f60384dedb8580f5f81c4e400
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2021-11-10 14:03:26 +0100

    feat: create <FormControl/> component

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,13 @@
+// ===============================================================
+
 export const formatCurrency = (num) => {
   return "$" + Number(num.toFixed(1)).toLocaleString() + "";
 };
+
+// ===============================================================
+
+export const capitalizeFirstLetter = (string) => {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
+// ===============================================================
