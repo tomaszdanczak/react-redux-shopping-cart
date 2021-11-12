98882f9d5bf3c4d9c10b5e1a4ee6114e1687e8d0
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2021-11-12 14:10:00 +0100

    feat: add parser express.json()

--- app/app.js
+++ app/app.js
@@ -4,6 +4,8 @@ const app = express();
 // init database
 require("./db/mongoose");
 
+app.use(express.json());
+
 // mount routes
 app.use(require("./routes/web.js"));
 app.use(require("./routes/api.js"));
