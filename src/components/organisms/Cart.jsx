9e5a8415ffa952a63894c20446bdbdbf0bc10bce
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2021-11-15 18:54:22 +0100

    fix: display content of the <Cart/> only when at least one item is in the cart

--- src/components/organisms/Cart.jsx
+++ src/components/organisms/Cart.jsx
@@ -7,12 +7,24 @@ export default function Cart() {
   const cartItems = useSelector((state) => state.cartState);
   const { isOrderFormOpen } = useSelector((state) => state.orderState);
 
+  const renderCart = () => {
+    if (cartItems.length === 0) {
+      return;
+    } else {
+      return (
+        <>
+          <CartItems />
+          <ProceedOrder />
+          {isOrderFormOpen && <CheckoutForm />}
+        </>
+      );
+    }
+  };
+
   return (
     <div className="col-span-2 ml-4 mr-4">
       <p className="mb-4 pb-4 border-b border-gray-500">{`You have ${cartItems.length} in the cart.`}</p>
-      <CartItems />
-      <ProceedOrder />
-      {isOrderFormOpen && <CheckoutForm />}
+      {renderCart()}
     </div>
   );
 }
