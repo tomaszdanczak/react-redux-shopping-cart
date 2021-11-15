ca1e56d23bd6107db318f1a3e0f58f1bbf352877
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2021-11-15 19:17:41 +0100

    feat: save state of form using useState

--- src/components/molecules/FormControl.jsx
+++ src/components/molecules/FormControl.jsx
@@ -1,6 +1,6 @@
 import { capitalizeFirstLetter } from "../../utils";
 
-export default function FormControl({ name, type, value }) {
+export default function FormControl({ name, type, value, onChangeInput }) {
   return (
     <div className="flex flex-col mb-4">
       <label className="mr-4" htmlFor={`form__${name}`}>
@@ -12,6 +12,7 @@ export default function FormControl({ name, type, value }) {
         id={`form__${name}`}
         required
         value={value}
+        onChange={onChangeInput}
         className="border border-gray-500 px-2 p-2"
       />
     </div>
--- src/components/organisms/CheckoutForm.jsx
+++ src/components/organisms/CheckoutForm.jsx
@@ -1,13 +1,35 @@
+import { useState } from "react";
 import Button from "../atoms/Button";
 import FormControl from "../molecules/FormControl";
 
 export default function CheckoutForm() {
+  const [form, setForm] = useState({ email: "", name: "", address: "" });
+
+  const handleChangeInput = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
   return (
     <div className="border-t border-gray-400 mt-14 pt-4 relative overflow-hidden">
       <form className="relative w-full h-full animate-fade-in">
-        <FormControl name="email" type="email" />
-        <FormControl name="name" type="text" />
-        <FormControl name="address" type="text" />
+        <FormControl
+          value={form.email}
+          onChangeInput={handleChangeInput}
+          name="email"
+          type="email"
+        />
+        <FormControl
+          value={form.name}
+          onChangeInput={handleChangeInput}
+          name="name"
+          type="text"
+        />
+        <FormControl
+          value={form.address}
+          onChangeInput={handleChangeInput}
+          name="address"
+          type="text"
+        />
         <Button
           text="Checkout"
           customStyle="w-full py-2 tracking-widest bg-yellow-300 border border-gray-900 hover:bg-yellow-400 active:bg-yellow-200"
