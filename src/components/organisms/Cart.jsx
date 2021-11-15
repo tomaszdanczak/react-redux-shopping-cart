07a9778be54cf5df6124f683acf3c00d8730f673
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2021-11-15 18:12:13 +0100

    fix: repair count of products from cart

--- src/components/organisms/Cart.jsx
+++ src/components/organisms/Cart.jsx
@@ -1,9 +1,11 @@
+import { useSelector } from "react-redux";
 import CartItems from "./CartItems";
-import { cartItems } from "./../../data.json";
 import ProceedOrder from "../molecules/ProceedOrder";
 import CheckoutForm from "./CheckoutForm";
 
 export default function Cart() {
+  const cartItems = useSelector((state) => state.cartState);
+
   return (
     <div className="col-span-2 ml-4 mr-4">
       <p className="mb-4 pb-4 border-b border-gray-500">{`You have ${cartItems.length} in the cart.`}</p>
