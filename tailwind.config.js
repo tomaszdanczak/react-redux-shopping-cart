345efeb6698a1984444582a64df88089551a014b
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2021-11-15 18:17:38 +0100

    fix: repair animations in <CartItem/>

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      animation: {
+        "fade-in": "fadeIn 1s ease-in-out",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { right: "-200px", opacity: 0.2 },
+          "100%": { right: "0px", opacity: 1 },
+        },
+      },
       gridTemplateRows: {
         layout: "3.125rem 1fr 3.125rem",
       },
